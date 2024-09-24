0324fdf0c8a6672364f163cd8535220a65d786ae
sorok <soroka144@gmail.com>
2024-09-24 11:40:49 +0300

    Наследование

---
+++ app/src/main/java/com/example/studyproject/OOP/Inheritance.kt
@@ -0,0 +1,27 @@
+package com.example.studyproject.OOP
+
+//Наследование
+
+open class Parent() {  // По умолчанию классы в котлин private, для наследования базового класса используется ключевое слово open
+    val firstNumberForExample = 10
+    open fun doSmthg() {
+        println("Do something")
+    }
+}
+
+class Child() : Parent() { // класс -наследник может взаимодействовать и расширять функции базового класа
+
+    fun printParentNumber(){
+        println("Parent " + Parent().firstNumberForExample)
+    }
+    override fun doSmthg() { // либо изменять поведение
+        println("Nope")
+    }
+}
+
+fun main() {
+    Child().doSmthg() //
+    println(Child().firstNumberForExample)
+    Child().printParentNumber()
+}
+
